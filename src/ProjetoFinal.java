6d776e4204403b6626bec29cd3728eeb637e868c
lmsathler <55558699+lmsathler@users.noreply.github.com>
2019-09-19 16:52:47 -0300

    Add files via upload

---
+++ src/ProjetoFinal.java
@@ -0,0 +1,50 @@
+public class ProjetoFinal {
+
+     public static void main (String [] args) {
+         Ave pato = new Ave ();
+         Ave galinha = new Ave ();
+         Mamifero vaca = new Mamifero ();
+         Mamifero morcego = new Mamifero ();
+
+         pato.setCorPena("Bege");
+         pato.setVoa(true);
+         pato.setSom("Grasnido");
+         pato.setIdade(3);
+         pato.setTamanho("Medio");
+         System.out.println ("O pato voa? " + (pato.isVoa() ? "Voa":"Nao voa"));
+         System.out.println ("O som do pato é : " + pato.getSom());
+         System.out.println ("A cor da pena do pato é : " + pato.getCorPena());
+         pato.mostraDados();
+            
+         galinha.setCorPena("Preto");
+         galinha.setVoa(false);
+         galinha.setSom ("Cacarejo");
+         galinha.setIdade(6);
+         galinha.setTamanho("Pequeno");
+         System.out.println ("A galinha voa? " + (galinha.isVoa() ? "Voa":"Nao voa"));
+         System.out.println ("O som da galinha é: " + galinha.getSom());
+         System.out.println ("A cor da pena da galinha é: " + galinha.getCorPena());
+         galinha.mostraDados();
+         
+         vaca.setCorPelo("Branca");
+         vaca.setVoa(false);
+         vaca.setSom("Mugido");
+         vaca.setIdade(4);
+         vaca.setTamanho("Grande");
+         System.out.println("O som da vaca é: " + vaca.getSom());
+         System.out.println ("A cor do pelo da vaca é: " + vaca.getCorPelo() );
+         vaca.mostraDados();
+         
+         morcego.setCorPelo("Marrom");
+         morcego.setVoa(true);
+         morcego.setSom("Farfalho");
+         morcego.setIdade(5);
+         morcego.setTamanho("Pequeno");
+         System.out.println("O morcego voa? " + (morcego.isVoa()? "Voa" : "Nao voa"));
+         System.out.println ("O som do morcego é: " + morcego.getSom());
+         System.out.println ("A cor do pelo do morcego é: "+ morcego.getCorPelo());
+         morcego.mostraDados();
+
+
+     }
+}
