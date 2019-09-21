94d3b4b25f172bf0604faa1f3a9c292412bf0fe6
lmsathler <55558699+lmsathler@users.noreply.github.com>
2019-09-20 21:46:17 -0300

    Add files via upload

---
+++ src/Ave.java
@@ -0,0 +1,18 @@
+public class Ave extends Animal{
+    private String corPena;
+    private boolean botaOvos;
+
+    public String getCorPena (){
+        return this.corPena;
+    }
+    public void setCorPena (String corPena){
+        this.corPena = corPena;
+    }
+    public boolean getBotaOvos () {
+        return this.botaOvos;
+    }
+    public void setBotaOvos (boolean botaOvos){
+        this.botaOvos = botaOvos;
+    }
+
+}
